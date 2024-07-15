fb42fa1ccdaa99a5ec61fd9c43e0a04cb6cd4e0a
Elias Arnold <eliasarnold@outlook.de>
2024-07-15 05:47:17 +0200

    feat: improve training

--- experiments/motion_detection.py
+++ experiments/motion_detection.py
@@ -1,6 +1,8 @@
 """ Train motion detection algoritm """
 import torch
+import torchvision
 import numpy as np
+from pathlib import Path
 from tqdm import tqdm
 from astrosite_dataset import BinaryClassificationAstrositeDataset
 from models.scnn_detection import MotionDetectionSNN
@@ -8,18 +10,32 @@ from tonic.slicers import SliceByTime
 from tonic import SlicedDataset, transforms
 
 
-lr = 1e-3
-epochs = 10
-batch_size = 128
+base_path = Path("./data/dynamic_tau")
+data_path = Path("data.csv")
+model_path = Path("model.pt")
+base_path.mkdir(exist_ok=True, parents=True)
+with open(base_path / data_path, "w") as file:
+    file.write("epochs;train_loss;train_acc;val_loss;val_acc\n")
+
+lr = 1e-5
+reg_strength = 1e-6
+epochs = 100
+batch_size = 64 
 device = torch.device("cuda") if torch.cuda.is_available() \
     else torch.device("cpu")
 dtype = torch.float
 loss_fn = torch.nn.BCEWithLogitsLoss()
 
 # Membrane time constant
-tau_mem1 = 10e-3
-tau_mem2 = 10e-3
-tau_mem3 = 20e-3
+tau_mem1 = 0.9
+tau_mem2 = 0.9
+tau_mem3 = 0.95
+train_mem = False
+
+# Target size for tonic down sampling
+target_size = [256, 144]
+# Additional down sampling
+input_avg_pooling = 1
 
 # data
 dataset_path = '/ley/users/earnold/datasets/astrosite/recordings'
@@ -31,6 +47,21 @@ target_list = [
 # Fix random seed
 np.random.seed(0)
 torch.manual_seed(0)
+
+
+class TransformedSubset(torch.utils.data.Dataset):
+    def __init__(self, subset, transform=None):
+        self.subset = subset
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x, y = self.subset[index]
+        if self.transform:
+            x = self.transform(x)
+        return x, y
+
+    def __len__(self):
+        return len(self.subset)
 
 
 def do_epoch(model, data_loader, optimizer, training: bool):
@@ -48,14 +79,14 @@ def do_epoch(model, data_loader, optimizer, training: bool):
         if training:
             optimizer.zero_grad()
 
-        # forward pass
+        # forward pass (one polarity)
         y = model(data)
         # sum-over-time
         y_sum = y.sum(1).reshape(-1)
 
         #  loss
-        loss = loss_fn(y_sum, target)
-        acc = torch.sum((y_sum > 0.5) == target)
+        loss = loss_fn(y_sum, target) + reg_strength * model.regularize()
+        acc = torch.sum((y_sum >= 0) == target)
 
         # Gradient calculation + weight update
         if training:
@@ -67,9 +98,10 @@ def do_epoch(model, data_loader, optimizer, training: bool):
         accs.append(acc.detach())
 
         # count samples
-        samples += data.shape[1]
+        samples += data.shape[0]
 
-        pbar.set_postfix(loss=f"{loss:.4f}", acc=f"{acc:.4f}")
+        pbar.set_postfix(
+            loss=f"{loss/data.shape[0]:.4f}", acc=f"{acc/data.shape[0]:.4f}")
         pbar.update()
 
     pbar.close()
@@ -85,13 +117,17 @@ def main():
         dataset_path, split=target_list)
 
     # Slice dataset into time frames
-    slicer = SliceByTime(time_window=1e6, include_incomplete=False)
-    frame_transform = transforms.ToFrame(
-        sensor_size=dataset.sensor_size, time_window=1e5,
-        include_incomplete=True)
+    slicer = SliceByTime(time_window=10e6, include_incomplete=False)
+    downsample = transforms.Compose([
+        # transforms.Denoise(filter_time=1e5),
+        transforms.Downsample(
+            sensor_size=dataset.sensor_size, target_size=target_size),
+        transforms.ToFrame(
+            sensor_size=target_size + [2], n_time_bins=50,
+            include_incomplete=True)])
     dataset = SlicedDataset(
         dataset, slicer=slicer, metadata_path='metadata/1',
-        transform=frame_transform)
+        transform=downsample)
 
     # Split into train and test set
     # TODO: Use val set instead of test set
@@ -99,8 +135,25 @@ def main():
     indices = torch.arange(size)
     train_indices = indices[:int(size * 0.8)]
     val_indices = indices[int(size * 0.8):]
-    train_set = torch.utils.data.Subset(dataset, indices=train_indices)
-    val_set = torch.utils.data.Subset(dataset, indices=val_indices)
+    train_transforms = torchvision.transforms.Compose([
+        torchvision.transforms.Lambda(lambda input: torch.tensor(input)),
+        torchvision.transforms.Lambda(lambda input: input[:, 1]),
+        torchvision.transforms.RandomHorizontalFlip(p=0.5),
+        torchvision.transforms.RandomVerticalFlip(p=0.5),
+        torchvision.transforms.RandomRotation(30),
+        torchvision.transforms.Lambda(lambda input: input.unsqueeze(1)),
+    ])
+    val_transforms = torchvision.transforms.Compose([
+        torchvision.transforms.Lambda(lambda input: torch.tensor(input)),
+        torchvision.transforms.Lambda(lambda input: input[:, 1]),
+        torchvision.transforms.Lambda(lambda input: input.unsqueeze(1)),
+    ])
+    train_subset = torch.utils.data.Subset(dataset, indices=train_indices)
+    train_set = TransformedSubset(
+        train_subset, transform=train_transforms)
+    val_subset = torch.utils.data.Subset(dataset, indices=val_indices)
+    val_set = TransformedSubset(
+        val_subset, transform=val_transforms)
 
     # Data
     train_loader = torch.utils.data.DataLoader(
@@ -109,7 +162,9 @@ def main():
         val_set, shuffle=False, batch_size=batch_size, num_workers=1)
 
     # Model
-    model = MotionDetectionSNN(720, 1280, tau_mem1, tau_mem2, tau_mem3)
+    model = MotionDetectionSNN(
+        *target_size, input_avg_pooling,  tau_mem1, tau_mem2, tau_mem3,
+        train_mem=train_mem)
     model.to(device)
 
     # Optimizer
@@ -119,12 +174,17 @@ def main():
     # Train and test
     train_losses, val_losses, train_accs, val_accs = [], [], [], []
     pbar = tqdm(total=epochs, unit="epoch")
-    for _ in range(epochs):
+    for epoch in range(epochs):
         # Train and evaluate
         train_loss, train_acc = do_epoch(
             model, train_loader, optimizer, True)
         val_loss, val_acc = do_epoch(
             model, val_loader, optimizer, False)
+
+        with open(base_path / data_path, "a") as file:
+            file.write(
+                f"{epoch};{train_loss.item()};{train_acc.item()};"
+                + f"{val_loss.item()};{val_acc.item()}\n")
 
         pbar.set_postfix(
             loss=f"{train_loss:.4f}", acc=f"{train_acc:.4f}",
@@ -136,6 +196,10 @@ def main():
         val_losses.append(val_loss)
         train_accs.append(train_acc)
         val_accs.append(val_acc)
+
+        # Save model
+        torch.save(model.to("cpu").state_dict(), base_path / model_path)
+        model.to(device)
     
     pbar.close()
 
--- models/scnn_detection.py
+++ models/scnn_detection.py
@@ -6,58 +6,90 @@ import snntorch as snn
 class MotionDetectionSNN(torch.nn.Module):
 
     def __init__(
-              self, width: int, height: int, tau_mem_1: float, tau_mem_2: float,
-              tau_mem_3: float):
+              self, width: int, height: int, input_pooling: int,
+              tau_mem_1: float, tau_mem_2: float, tau_mem_3: float,
+              record: bool = False, train_mem: bool = False):
         super().__init__()
-        kernel_size = 5
-        pooling_size = 2
-        stride = 4
 
-        out_shape = torch.tensor([width, height])
+        self.record = record
+        kernel_size1 = 5
+        kernel_size2 = 5
+        pooling_size = 4
+        stride = 1
+
+        out_shape = torch.tensor([width, height], dtype=int)
+
+        sg = snn.surrogate.fast_sigmoid(slope=50)
+
+        # Down sample 
+        self.pool0 = torch.nn.AvgPool2d(input_pooling)
+        out_shape = (out_shape / input_pooling).to(int)
 
         # conv block 1
         self.conv1 = torch.nn.Conv2d(
-            2, 4, kernel_size, padding=int(kernel_size//2), stride=stride)
+            1, 8, kernel_size1, padding=int(kernel_size1//2), stride=stride)
         self.pool1 = torch.nn.AvgPool2d(pooling_size)
-        out_shape = out_shape / stride / pooling_size
-        self.lif1 = snn.Leaky(beta=tau_mem_1, output=True)
+        out_shape = (out_shape / stride / pooling_size).to(int)
+        self.lif1 = snn.Leaky(
+            beta=tau_mem_1, output=True, spike_grad=sg, learn_beta=train_mem)
 
         # conv block 2
         self.conv2 = torch.nn.Conv2d(
-            4, 20, kernel_size, padding=int(kernel_size//2), stride=stride)
+            8, 8, kernel_size2, padding=int(kernel_size2//2), stride=stride)
         self.pool2 = torch.nn.AvgPool2d(pooling_size)
-        out_shape = out_shape / stride / pooling_size
-        self.lif2 = snn.Leaky(beta=tau_mem_2, output=True)
+        out_shape = (out_shape / stride / pooling_size).to(int)
+        self.lif2 = snn.Leaky(
+            beta=tau_mem_2, output=True, spike_grad=sg, learn_beta=train_mem)
+        print(out_shape)
 
         # ff block
         self.linear = torch.nn.Linear(
-            20 * int(out_shape[0]) * int(out_shape[1]), 1, bias=False)
+            8 * int(out_shape[0]) * int(out_shape[1]), 1, bias=False)
         self.li = snn.Leaky(beta=tau_mem_3, threshold=100000)
+
+    def regularize(self):
+        reg_loss = 0.
+        reg_loss += torch.mean(self.z1.sum(1)**2)
+        reg_loss += torch.mean(self.z2.sum(1)**2)
+        return reg_loss
 
     def forward(self, x):
         self.lif1.reset_mem()
+        self.lif1.beta.data = torch.max(torch.tensor(0.99), self.lif1.beta.data)
         self.lif2.reset_mem()
+        self.lif2.beta.data = torch.max(torch.tensor(0.99), self.lif2.beta.data)
         self.li.reset_mem()
 
         zs1, vs1, zs2, vs2, ys = [], [], [], [], []
         for ts in range(x.shape[1]):
+
+            # Down sample
+            x0 = self.pool0(x[:, ts])
+
             # conv block 1
-            g1 = self.conv1(x[:, ts])
-            p1 = self.pool1(g1)
-            z1, v1 = self.lif1(p1)
+            g1 = self.conv1(x0)
+            z1, v1 = self.lif1(g1)
+            p1 = self.pool1(z1)
             zs1.append(z1)
             vs1.append(v1)
 
             # conv block 2
-            g2 = self.conv2(z1)
-            p2 = self.pool2(g2)
-            z2, v2 = self.lif2(p2)
+            g2 = self.conv2(p1)
+            z2, v2 = self.lif2(g2)
+            p2 = self.pool2(z2)
             zs2.append(z2)
             vs2.append(v2)
 
             # ff block
-            g3 = self.linear(torch.flatten(z2, start_dim=1))
+            g3 = self.linear(torch.flatten(p2, start_dim=1))
             _, y = self.li(g3)
             ys.append(y)
+
+        self.z1 = torch.stack(zs1, dim=0).transpose(1, 0)
+        self.z2 = torch.stack(zs2, dim=0).transpose(1, 0)
+        if self.record:
+            self.v1 = torch.stack(vs1, dim=0).transpose(1, 0)
+            self.v2 = torch.stack(vs2, dim=0).transpose(1, 0)
+            self.y = torch.stack(ys, dim=0).transpose(1, 0)
 
         return torch.stack(ys, dim=0).transpose(1, 0)
