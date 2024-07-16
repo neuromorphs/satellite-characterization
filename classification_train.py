2325113fa3aac0559f191b785ba94c0c2fb43e9c
lecomtefortiss <lecomte@fortiss.org>
2024-07-16 10:47:56 -0600

    training with news classes

--- classification_train.py
+++ classification_train.py
@@ -8,8 +8,7 @@ from classification_model import SpectrogramCNN
 
 def main(args):
     data_module = ClassificationDataset(batch_size=args.batch_size, dataset_path=args.dataset_path)
-
-    class_names = ["39483", "39485", "39624", "40921", "43751", "43752", "50574", "51102"]
+    class_names = ["50574","23230"]
     model = SpectrogramCNN(num_classes=args.num_classes, class_names=class_names)
 
     # Callbacks
@@ -20,7 +19,6 @@ def main(args):
         save_top_k=1,
         mode='min'
     )
-
     logger = TensorBoardLogger("tb_logs", name="spectrogram_cnn")
 
     trainer = Trainer(
@@ -35,8 +33,8 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a Spectrogram CNN with PyTorch Lightning')
     parser.add_argument('--batch_size', type=int, default=16, help='Batch size for training (default: 32)')
-    parser.add_argument('--dataset_path', type=str, default="data/astrosite/spectrograms", help='Path to dataset (default: data/astrosite/spectrograms_training)')
-    parser.add_argument('--num_classes', type=int, default=8, help='Number of classes (default: 5)')
+    parser.add_argument('--dataset_path', type=str, default="data/astrosite/spectrograms_training", help='Path to dataset (default: data/astrosite/spectrograms_training)')
+    parser.add_argument('--num_classes', type=int, default=2, help='Number of classes (default: 5)')
     parser.add_argument('--checkpoint_dir', type=str, default='checkpoints', help='Directory to save checkpoints (default: checkpoints)')
     parser.add_argument('--max_epochs', type=int, default=10, help='Maximum number of epochs (default: 10)')
 
